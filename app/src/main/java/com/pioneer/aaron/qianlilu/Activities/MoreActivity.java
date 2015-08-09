b2e0d605e1a6e4906bd5848716b8c35d3969d0d2
Aaron <pioneervon@gmail.com>
2015-08-09 21:50:10 +0800

    more setting & about activity

---
+++ app/src/main/java/com/pioneer/aaron/qianlilu/Activities/MoreActivity.java
@@ -0,0 +1,18 @@
+package com.pioneer.aaron.qianlilu.Activities;
+
+import android.os.Bundle;
+
+import com.pioneer.aaron.qianlilu.R;
+
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
+/**
+ * Created by Aaron on 8/9/15.
+ */
+public class MoreActivity extends SwipeBackActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_layout);
+    }
+}
---
+++ app/src/main/java/com/pioneer/aaron/qianlilu/Fragments/InboxFragment.java
@@ -0,0 +1,7 @@
+package com.pioneer.aaron.qianlilu.Fragments;
+
+/**
+ * Created by Aaron on 8/9/15.
+ */
+public class InboxFragment {
+}
